81c02b12336b29a1133289e8cffdec9cf2abb5fb
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-04-26 21:52:45 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-console.log('hi');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let timerId = null;
+
+function onStartClick() {
+  console.log('hi');
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
